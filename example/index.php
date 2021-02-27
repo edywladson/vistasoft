477998759bb90aa15251cab3894eeb5be20658f8
Edy Wladson <edywladson@gmail.com>
2021-02-27 17:39:26 -0300

    Inserido novos métodos e atributos e feito correções e ajustes

--- example/index.php
+++ example/index.php
@@ -1,15 +1,136 @@
 <?php
 require __DIR__ . "/../vendor/autoload.php";
 
+use EdyWladson\VistaSoft\VistaSoft;
 
-$vista = new \EdyWladson\VistaSoft\VistaSoft("http://sandbox-rest.vistahost.com.br", "c9fdd79584fb8d369a6a579af1a8f681");
+$vista = new VistaSoft("http=>//sandbox-rest.vistahost.com.br", "c9fdd79584fb8d369a6a579af1a8f681");
+
+//CLIENT LIST [LISTAR CLIENTES]
+//$get = $vista
+//    ->endpoint("/clientes/listar")
+//    ->fields(["Codigo", "Nome"])
+//    ->get()->callback();
+//var_dump($get);
+
+//CLIENT DETAILS [DETALHES DO CLIENTE]
+//$get = $vista
+//    ->endpoint("/clientes/detalhes")
+//    ->clientId(53016)
+//    ->fields(["Codigo", "Nome"])
+//    ->get()
+//    ->callback();
+//var_dump($get);
+
+//CLIENT DETAILS (HISTORY) [DETALHES DO CLIENTE (HISTÓRICOS)]
+//$get = $vista
+//    ->endpoint("/clientes/detalhes")
+//    ->clientId(73)
+//    ->fields(["Codigo", "Nome", ['historicos' => ["Data","Hora","Imovel","Assunto","Texto"]]])
+//    ->get()
+//    ->callback();
+//var_dump($get);
+
+//CLIENT DETAILS (AGENCY) [DETALHES DO CLIENTE (AGÊNCIA)]
+//$get = $vista
+//    ->endpoint("/clientes/detalhes")
+//    ->clientId(73)
+//    ->fields(["Codigo", "Nome", ['Agencia' => ["Nome","Endereco"]]])
+//    ->get()
+//    ->callback();
+//var_dump($get);
+
+//CLIENT POST [CADASTRAR CLIENTE]
+//$post = $vista
+//    ->endpoint("/clientes/detalhes")
+//    ->fields(["Nome" => "Vista Api", "FonePrincipal" => "00 0000 0000", "VeiculoCaptacao" => "Portal 10"])
+//    ->post();
+//
+//var_dump($post);
+
+//CLIENT PUT [ATUALIZAR CLIENTE]
+//$put = $vista
+//    ->endpoint("/clientes/detalhes")
+//    ->clientId(73)
+//    ->fields(["FoneResidencial" => "2186530000"])
+//    ->put()->callback();
+//
+//var_dump($put);
+
+//SEND LEADS [ENVIAR LEADS]
+//$put = $vista
+//    ->endpoint("/lead")
+//    ->leads(["nome"=>"Teste de cadastro","fone"=>"77 9988.7756","email"=>"teste@apivista.com.br","mensagem"=>"Tenho interesse no imóvel","veiculo"=>"Veiculo de Captação","interesse"=>"venda"])
+//    ->post()->callback();
+//
+//var_dump($put);
+
+
+//IMMOBILE LIST [LISTA DE IMÓVEIS]
 $get = $vista
-    ->fields(["Codigo", "Nome"])
-    ->endpoint("/clientes/listar")
-    ->paginator()
-    ->get()
-    ->callback();
-
+    ->endpoint("/imoveis/listar")
+    ->fields(["Codigo", "Bairro"])
+    ->get();
 var_dump($get);
 
+//FILTER IMMOBILE LIST [LISTA DE IMÓVEIS COM FILTRO]
+//$get = $vista
+//    ->endpoint("/imoveis/listar")
+//    ->fields(["Codigo", "Bairro"])
+//    ->filter(["Bairro" => ["Centro", "Moema"]])
+//    ->order(["Bairro" => "asc"])
+//    ->get()->callback();
+//var_dump($get);
 
+//LIST CONTENTS [LISTA DE CONTEÚDOS]
+//$get = $vista
+//    ->endpoint("/imoveis/listarConteudo")
+//    ->fields(["Bairro"])
+//    ->get()->callback();
+//var_dump($get);
+
+//IMMOBILE DETAILS [DETALHES DO IMÓVEL]
+//$get = $vista
+//    ->endpoint("/imoveis/detalhes")
+//    ->immobileId(3152)
+//    ->fields(["Codigo","Categoria","Bairro","Cidade","ValorVenda","ValorLocacao"])
+//    ->get()->callback();
+//var_dump($get);
+
+//IMMOBILE PHOTOS [FOTOS DO IMÓVEL]
+//$get = $vista
+//    ->endpoint("/imoveis/detalhes")
+//    ->immobileId(3152)
+//    ->fields(["Codigo","Categoria","Bairro","Cidade","ValorVenda","ValorLocacao", ['Foto' => ['Foto','FotoPequena','Destaque']]])
+//    ->get()->callback();
+//var_dump($get);
+
+//IMMOBILE ATTACHMENTS [ANEXOS IMÓVEL]
+//$get = $vista
+//    ->endpoint("/imoveis/detalhes")
+//    ->immobileId(3152)
+//    ->fields(["Codigo","Categoria","Bairro","Cidade","ValorVenda","ValorLocacao", ['Anexo' => ['Anexo','Descricao']]])
+//    ->get()->callback();
+//var_dump($get);
+
+//IMMOBILE HISTORY [HISTÓRICO DO IMÓVEL]
+//$get = $vista
+//    ->endpoint("/imoveis/detalhes")
+//    ->immobileId(3152)
+//    ->fields(["Codigo","Categoria","Bairro","Cidade","ValorVenda","ValorLocacao", ['prontuarios' => ["Data","Hora","Assunto","Texto"]]])
+//    ->get()->callback();
+//var_dump($get);
+
+//IMMOBILE POST [CADASTRAR UM IMÓVEL]
+//$post = $vista
+//    ->endpoint("/imoveis/detalhes")
+//    ->fields(["Categoria"=>"Apartamento"])
+//    ->post()->callback();
+//var_dump($post);
+
+//IMMOBILE PUT [ATUALIZAR UM IMÓVEL]
+//$post = $vista
+//    ->endpoint("/imoveis/detalhes")
+//    ->immobileId(3152)
+//    ->fields(["Categoria"=>"Apartamento"])
+//    ->post()->callback();
+//var_dump($post);
--- src/VistaSoft.php
+++ src/VistaSoft.php
@@ -12,10 +12,25 @@ class VistaSoft
 {
     /** @var string */
     private $apiUrl;
+
     /** @var string */
     private $apiKey;
+
+    /** @var string */
+    private $endpoint;
+
     /** @var array */
     private $build;
+
+    /** @var string */
+    private $showtotal;
+
+    /** @var string */
+    private $immobileId;
+
+    /** @var string */
+    private $clientId;
+
     /** @var mixed */
     private $callback;
 
@@ -44,15 +59,34 @@ class VistaSoft
     }
 
     /**
+     * @param array $leads
+     * @return $this
+     */
+    public function leads(array $leads): VistaSoft
+    {
+        $this->build = [
+            "lead" => $leads
+        ];
+
+        return $this;
+    }
+
+    /**
      * @param array $filterBy
      * @return $this
      */
     public function filter(array $filterBy): VistaSoft
     {
+        if(empty($this->build)){
+            $this->build = [
+                "filter" => $filterBy
+            ];
+            return $this;
+        }
+
         $this->build += [
             "filter" => $filterBy
         ];
-
         return $this;
     }
 
@@ -75,11 +109,10 @@ class VistaSoft
      * @param bool $total
      * @return $this
      */
-    public function paginator(int $page = 1, int $quantity = 10, bool $total = false): VistaSoft
+    public function paginator(int $page = 1, int $quantity = 20, bool $total = false): VistaSoft
     {
-        $showtotal = ($total) ? "&showtotal=1" : '';
+        $this->showtotal = ($total) ? "&showtotal=1" : '';
         $this->build += [
-            $showtotal,
             "paginacao" => [
                 "pagina" => $page,
                 "quantidade" => $quantity
@@ -90,12 +123,32 @@ class VistaSoft
     }
 
     /**
+     * @param int $id
+     * @return $this
+     */
+    public function clientId(int $id): VistaSoft
+    {
+        $this->clientId = "&cliente={$id}";
+        return $this;
+    }
+
+    /**
+     * @param int $id
+     * @return $this
+     */
+    public function immobileId(int $id): VistaSoft
+    {
+        $this->immobileId = "&imovel={$id}";
+        return $this;
+    }
+
+    /**
      * @param string $endpoint
      * @return $this
      */
     public function endpoint(string $endpoint): VistaSoft
     {
-        $this->apiUrl = "{$this->apiUrl}{$endpoint}";
+        $this->endpoint = $endpoint;
         return $this;
     }
 
@@ -113,13 +166,63 @@ class VistaSoft
      */
     public function get(): VistaSoft
     {
-        $url = "{$this->apiUrl}?key={$this->apiKey}";
+        $url = "{$this->apiUrl}{$this->endpoint}?key={$this->apiKey}";
         $fields = json_encode($this->build);
-        $url .= "&pesquisa={$fields}";
+        $url .= "&pesquisa={$fields}{$this->showtotal}{$this->clientId}{$this->immobileId}";
 
         $ch = curl_init($url);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
         curl_setopt($ch, CURLOPT_HTTPHEADER, ['Accept: application/json']);
+        $this->callback = json_decode(curl_exec($ch));
+        curl_close($ch);
+        return $this;
+    }
+
+    /**
+     * POST
+     * @return $this
+     */
+    public function post(): VistaSoft
+    {
+        $url = "{$this->apiUrl}{$this->endpoint}?key={$this->apiKey}";
+        $fields = "cadastro=" . json_encode($this->build);
+        $url .= "{$this->clientId}{$this->immobileId}";
+
+        $ch = curl_init($url);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $fields);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, [
+            'Content-type: application/x-www-form-urlencoded',
+            'Accept: application/json',
+            'Content-Length: ' . strlen($fields)
+        ]);
+        $this->callback = json_decode(curl_exec($ch));
+        curl_close($ch);
+        return $this;
+    }
+
+    /**
+     * PUT
+     * @return $this
+     */
+    public function put(): VistaSoft
+    {
+        $url = "{$this->apiUrl}{$this->endpoint}?key={$this->apiKey}";
+        $fields = "cadastro=" . json_encode($this->build);
+        $url .= "{$this->clientId}{$this->immobileId}";
+
+        var_dump($fields);
+
+        $ch = curl_init($url);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "PUT");
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $fields);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, [
+            'Content-type: application/x-www-form-urlencoded',
+            'Accept: application/json',
+            'Content-Length: ' . strlen($fields)
+        ]);
         $this->callback = json_decode(curl_exec($ch));
         curl_close($ch);
         return $this;
